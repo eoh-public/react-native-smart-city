135dab767e0141d09c641192eaa32784b3a930b1
Hien Nguyen <hienn2006@gmail.com>
2021-04-10 16:56:29 +0700

    .eslintrc.js jest.config.js,  mock, scripts

---
+++ jest.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '\\.svg': '<rootDir>/__mocks__/svgMock.js',
+  },
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      statements: 70.04,
+      branches: 64.91,
+      functions: 60.05,
+      lines: 70.68,
+    },
+  },
+  modulePathIgnorePatterns: ['SampleProject'],
+  coveragePathIgnorePatterns: ['/Images/', '/\\.json/'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+    '!src/libs/**',
+  ],
+  transformIgnorePatterns: ['node_modules/(?!react\b)\bw+\b'],
+  setupFiles: [
+    'dotenv/config',
+    './jest/setup.js',
+    './jest/react-native-reanimated.setup.js',
+    './jest/react-native-stripe.setup.js',
+    './node_modules/react-native-gesture-handler/jestSetup.js',
+  ],
+  setupFilesAfterEnv: ['./jest/test-setup.js'],
+  globalSetup: './jest/global-setup.js',
+  testEnvironment: 'jsdom',
+};
