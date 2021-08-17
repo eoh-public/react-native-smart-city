40050419b7f19f72ee7f2a3d7e6ae1dd7ac19be6
HinHinDA <hinh.dinh86197@gmail.com>
2021-08-17 21:08:02 +0700

    Fix usIsOwner

--- src/hooks/Common/useIsOwnerOfUnit.js
+++ src/hooks/Common/useIsOwnerOfUnit.js
@@ -2,8 +2,10 @@ import { useSCContextSelector } from '../../context';
 
 // this hook is using for the screen have unit.user_id in route
 export const useIsOwnerOfUnit = (unitOwnerId) => {
-  const token = useSCContextSelector(state => state?.auth?.account?.token)
-  let isOwner = token === unitOwnerId;
+  const idUser = useSCContextSelector(
+    (state) => state?.auth?.account?.user?.id
+  );
+  let isOwner = idUser === unitOwnerId;
 
   return {
     isOwner,
