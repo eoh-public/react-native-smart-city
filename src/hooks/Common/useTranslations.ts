9401960f608dcbc3e711fdb660468d8afc8b1a85
Nam Phương <nam.np@eoh.io>
2021-09-15 14:01:21 +0000

    fix crash app useSelector

--- src/hooks/Common/useTranslations.ts
+++ src/hooks/Common/useTranslations.ts
@@ -6,22 +6,25 @@ import {
 } from '../../utils/I18n';
 import { useCallback } from 'react';
 import { useSCContextSelector } from '../../context';
-
-// eslint-disable-next-line react-hooks/rules-of-hooks
-const lang = useSCContextSelector((state) => state.language);
-
-const language: Language = lang as Language;
+import { getConfigGlobalState } from '../../iot/states.js';
 
 export const useTranslations = () => {
+  const lang = useSCContextSelector((state) => state.language);
+
+  const language: Language = lang as Language;
+
   const t = useCallback(
     (translations: TranslationString, interpolateStr?: InterPolateString) =>
       getTranslate(language, translations, interpolateStr),
-    []
+    [language]
   );
 
   return t;
 };
 
-const t = (text) => getTranslate(language, text);
+const t = (text) => {
+  const lang = getConfigGlobalState('lang');
+  getTranslate(lang, text);
+};
 
 export default t;
