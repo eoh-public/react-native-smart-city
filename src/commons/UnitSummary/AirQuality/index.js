c79e673a5df9dbd162a2bb453a5fb9bd121d9942
NhatPham <nhat.pm@eoh.io>
2021-09-20 10:55:39 +0700

    remove residual touch

--- src/commons/UnitSummary/AirQuality/index.js
+++ src/commons/UnitSummary/AirQuality/index.js
@@ -1,5 +1,5 @@
 import React, { memo, useCallback, useMemo, useState } from 'react';
-import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
 import { IconOutline } from '@ant-design/icons-react-native';
 import { useTranslations } from '../../../hooks/Common/useTranslations';
 
@@ -10,6 +10,7 @@ import ConfigHistoryChart from '../ConfigHistoryChart';
 
 import SegmentedRoundDisplay from './SegmentedRoundDisplay';
 import { TESTID } from '../../../configs/Constants';
+import styles from './styles';
 
 const AirQuality = memo(({ summaryDetail }) => {
   const t = useTranslations();
@@ -124,14 +125,6 @@ const AirQuality = memo(({ summaryDetail }) => {
             style={styles.segment}
             textHeader={t('text_air_quality_index')}
           />
-
-          <TouchableOpacity
-            style={[styles.touchStatus, { backgroundColor: outdoorColor }]}
-          >
-            <Text size={14} color={Colors.White}>
-              {outdoorStatus}
-            </Text>
-          </TouchableOpacity>
           {!!advices && !!advices.length && (
             <View style={styles.boxHealth}>
               <IconOutline name="alert" size={20} style={styles.iconMargin} />
@@ -175,113 +168,3 @@ const AirQuality = memo(({ summaryDetail }) => {
 });
 
 export default AirQuality;
-
-const styles = StyleSheet.create({
-  textIndoor: {
-    color: Colors.Gray9,
-    fontSize: 20,
-    marginBottom: 4,
-  },
-  textDesIndoor: {
-    color: Colors.Gray8,
-    fontSize: 12,
-    marginBottom: 16,
-  },
-  textNotAvailable: {
-    textAlign: 'center',
-  },
-  boxStatus: {
-    borderRadius: 10,
-    height: 80,
-    overflow: 'hidden',
-    flexDirection: 'row',
-    marginBottom: 16,
-  },
-  boxEmotion: {
-    height: 80,
-    width: 80,
-    backgroundColor: Colors.Yellow6,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  textStatus: {
-    fontSize: 16,
-    color: Colors.Gray9,
-    marginRight: 24,
-    textAlign: 'right',
-    maxWidth: 120,
-  },
-  boxTextStatus: {
-    backgroundColor: Colors.Yellow4,
-    flex: 1,
-    alignItems: 'flex-end',
-    justifyContent: 'center',
-  },
-  boxNumber: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 16,
-  },
-  boxHealth: {
-    flexDirection: 'row',
-    marginBottom: 12,
-    alignItems: 'center',
-    marginTop: 10,
-  },
-  boxContentHealth: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 8,
-  },
-  boxDot: {
-    width: 8,
-    height: 8,
-    borderRadius: 4,
-    backgroundColor: Colors.Yellow6,
-    marginRight: 8,
-  },
-  textOutdoor: {
-    color: Colors.Gray9,
-    fontSize: 20,
-    marginBottom: 24,
-    marginTop: 16,
-  },
-  touchOption: {
-    borderWidth: 1,
-    borderColor: Colors.Gray5,
-    paddingHorizontal: 16,
-    paddingVertical: 5,
-    marginHorizontal: 4,
-    borderRadius: 2,
-  },
-  textOption: {
-    alignSelf: 'center',
-    marginBottom: 8,
-  },
-  touchStatus: {
-    borderRadius: 100,
-    paddingHorizontal: 24,
-    alignSelf: 'center',
-    paddingVertical: 3,
-    marginTop: 12,
-  },
-  segment: {
-    marginTop: 36,
-    alignSelf: 'center',
-  },
-  boxOutdoorValues: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
-  iconMargin: {
-    marginRight: 8,
-  },
-  historyChart: {
-    paddingHorizontal: 16,
-    backgroundColor: Colors.White,
-    borderRadius: 20,
-    borderWidth: 1,
-    borderColor: Colors.Gray4,
-    marginTop: 16,
-  },
-});
